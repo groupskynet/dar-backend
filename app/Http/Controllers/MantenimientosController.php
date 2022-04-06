37982fa43b2ad2656751e4c1b4e75f3f5e822bae
Alberto Rojas <ing.beto.rojas@gmail.com>
2022-04-06 10:32:37 -0500

    se creo crud de mantenimientos

---
+++ app/Http/Controllers/MantenimientosController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mantenimiento;
+use App\Models\Mantenimientos;
+use App\Models\Proveedores;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+
+class MantenimientosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $mantenimientos = Mantenimientos::with('proveedor','maquina')->paginate(10);
+        return response()->json([
+            'status'=>Response::HTTP_OK,
+            'message'=>'success',
+            'data'=>$mantenimientos
+        ],Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validate = Validator::make($request->all(),[
+            'tipo'=>'required',
+            'maquina'=>'numeric|required',
+            'proveedor'=> 'numeric|required',
+            'horometro'=>'numeric|required',
+            'descripcion'=>'required',
+            'modalidad'=>'required',
+            'costo'=>'required',
+            'soporte'=>'required|mimes:pdf'
+
+        ]);
+        if($validate->fails()){
+            return response()->json([
+                'status'=>Response::HTTP_BAD_REQUEST,
+                'message'=>'invalid data',
+                'data'=> $validate->errors()
+            ],Response::HTTP_OK);
+        }
+
+        $path='';
+
+        if($request->hasFile('soporte')){
+            $path = $request->file('soporte')->storeAs(
+                'soportes', 'soporte-'. $request->proveedor . '-' . date('Y-m-d-hh:mm') . '-' . $request->file('soporte')->getClientOriginalName()
+            );
+        }
+
+        $mantenimiento = new Mantenimientos($request->all());
+        $mantenimiento->soporte = $path;
+        $mantenimiento->descripcion = strtoupper($request->decripcion);
+        $resullt = $mantenimiento->save();
+
+        if($resullt){
+            return response()->json([
+                'status'=>Response::HTTP_OK,
+                'mesage'=>'Datos guardados correctamente'
+            ], Response::HTTP_OK);
+        }
+        return response()->json([
+            'status'=>Response::HTTP_INTERNAL_SERVER_ERROR,
+            'message'=> 'Error de servidor'
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Mantenimiento  $mantenimiento
+     * @return \Illuminate\Http\Response
+     */
+    
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Mantenimiento  $mantenimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mantenimientos $mantenimiento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Mantenimiento  $mantenimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Mantenimientos $mantenimiento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Mantenimiento  $mantenimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mantenimientos $mantenimiento)
+    {
+        //
+    }
+}
